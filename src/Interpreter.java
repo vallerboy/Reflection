927c36ba363b9ed9bb024a6294738a1a55bb7d1d
Oskar Pol <oskarbank@gmail.com>
2017-03-29 19:49:55 +0200

    Interperter

---
+++ src/Interpreter.java
@@ -0,0 +1,24 @@
+import java.lang.reflect.Method;
+
+public class Interpreter {
+
+    public static void main(String[] args) {
+        // Miłego kodzenia Oskar!
+
+        Interpreter interpreter = new Interpreter();
+        interpreter.runUsingInternet();
+    }
+
+    public void runUsingInternet() {
+       Player player = new Player();
+
+        Method[] methods = player.getClass().getDeclaredMethods();
+        for(Method method : methods){
+            if(method.isAnnotationPresent(UsingInternet.class)) {
+                 System.out.println("Metoda " + method.getName() + " korzysta z internetu");
+            }
+        }
+
+    }
+
+}
--- src/Player.java
+++ src/Player.java
@@ -6,13 +6,18 @@ public class Player {
     public String name;
     private String lastname;
 
-
+    @UsingInternet
     public String getName(){
         return name;
     }
 
-    private int getScore(){
+    public int getScore(){
         return score;
+    }
+
+    @UsingInternet
+    public void setName(String name) {
+        this.name = name;
     }
 
     public void printScore(String s) {
